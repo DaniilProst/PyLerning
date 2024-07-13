ae91a8c4f5f69f6bb509fbfe8f2afd502dbeaa18
DaniilProst <daniil-prost@mail.ru>
2024-07-13 14:48:49 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+hw_value = 12
+hs_value = 1.5
+course_name = "Python"
+time_for_tusk = 1.5/12
+
+print("Курс: "+course_name+",", "всего задач:",hw_value, ",", "затрачено часов:", hs_value,",", "Среднее время выполнения:", time_for_tusk, ".")
