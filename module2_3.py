79bfcf6dfa44ec7ddef9e9c107fbeb27bd52b6d0
DaniilProst <daniil-prost@mail.ru>
2024-08-06 18:39:06 +0300

    Add files via upload

--- module2_3.py
+++ module2_3.py
@@ -1,6 +1,6 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, 5, 9, 8, 7, 6, 5]
 index = 0
-while my_list[index] >= 0 and index < len(my_list):
+while index < len(my_list) and my_list[index] >= 0:
     if my_list[index] == 0:
         index += 1
         continue
