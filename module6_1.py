4759a2caec18ae84d4d751672bb364962af0b2d4
DaniilProst <daniil-prost@mail.ru>
2024-09-22 21:10:57 +0300

    Add files via upload

--- module6_1.py
+++ module6_1.py
@@ -4,6 +4,14 @@ class Animal:
         self.alive = True
         self.fed = False
 
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
 
 class Plant:
     def __init__(self, name):
@@ -23,24 +31,13 @@ class Flower(Plant):
 
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    def __init__(self, name):
+        super().__init__(name)
 
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
-
+    def __init__(self, name):
+        super().__init__(name)
 
 
 a1 = Predator('Волк с Уолл-Стрит')
